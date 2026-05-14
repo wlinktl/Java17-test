77a31b87afd3c49894c167869dac70f1c1de0e78
wlinktl <wlinkcl@outlook.com>
2026-05-14 11:16:59 -0400

    Update filter.java

--- filter.java
+++ filter.java
@@ -7,3 +7,32 @@ public FilterRegistrationBean<MultipartFilter> multipartFilterRegistrationBean()
     registrationBean.addUrlPatterns("/springrest/*");
     return registrationBean;
 }
+
+
+@SpringBootApplication
+// Must extend this class for external Tomcat to find your config
+public class MyWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // This replaces the contextConfigLocation param from image_87499c.png
+        // It tells Spring to include TestWebConfigurer in the startup
+        return builder.sources(MyWebApplication.class, TestWebConfigurer.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWebApplication.class, args);
+    }
+}
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.yourproject")
+public class TestWebConfigurer implements WebMvcConfigurer {
+    // Your JSP view resolver and other bean definitions here
+}
+
+
+
+
